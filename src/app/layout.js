4b1bd23685022799ba3a35f162e4a260c812b051
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-11-08 22:42:19 +0530

    logo kya kahenge

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-            <head>
+      <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Space+Grotesk:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/logo.png" type="image/png" />
       </head>
       <body>
         <Navbar />
